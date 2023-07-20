1f15d34b480537c0e7fb33395f967cbd0383f4e0
Eric Triebe <eric.triebe@gmail.com>
2023-07-20 16:04:06 -0700

    Redo change for PR #182 in 3.0.0

--- scripts/token-mold.js
+++ scripts/token-mold.js
@@ -395,6 +395,10 @@ export default class TokenMold {
       name = "";
     }
 
+    if ("prefix" === this.data.name.replace && !(this.data.name.baseNameOverride && event.getModifierState("Shift"))) {
+      name = name.substring(0, 2).toUpperCase();
+    }
+
     let numberSuffix = "";
     if (this.data.name.number.use) {
       let number = 0;
